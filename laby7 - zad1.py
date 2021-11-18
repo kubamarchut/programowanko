59597eb18d91e395f5f77e3872a50c74a1eba971
kubamarchut <43681491+kubamarchut@users.noreply.github.com>
2021-11-18 18:48:38 +0100

    Add files via upload

--- laby7 - zad1.py
+++ laby7 - zad1.py
@@ -1,52 +1,91 @@
-def czescWspolna(tab1, tab2):
+def commonpart(list1, list2):
     res = []
-    for i in tab1:
-        for j in tab2:
+    for i in list1:
+        for j in list2:
             if i == j:
                 res.append(i)
     return res
 
-def suma(tab1, tab2):
+def both(list1, list2):
     res = []
-    for i in tab1:
+    for i in list1:
         if i not in res:
             res.append(i)
 
-    for i in tab2:
+    for i in list2:
         if i not in res:
             res.append(i)
     return res
 
-def roznica(tab1, tab2):
+def difference(list1, list2):
     res = []
-    for i in tab1:
-        dontadd = False
-        for j in tab2:
+    for i in list1:
+        reapets = False
+        for j in list2:
             if i == j:
-                dontadd = True
-        if not dontadd:
+                reapets = True
+        if not reapets:
             res.append(i)
-            dontadd = False
+            reateps = False
     return res
 
-def zmienNaTablice(tekst):
-    return tekst[1:-1].split(',')
+def convertToList(text):
+    return text[1:-1].split(',')
 
-def operacjeNaZbiorach(polecenie):
-    if '*' in polecenie:
-        tablica1 = zmienNaTablice(polecenie.split('*')[0])
-        tablica2 = zmienNaTablice(polecenie.split('*')[1])
-        print(czescWspolna(tablica1, tablica2))
-    elif '+' in polecenie:
-        tablica1 = zmienNaTablice(polecenie.split('+')[0])
-        tablica2 = zmienNaTablice(polecenie.split('+')[1])
-        print(suma(tablica1, tablica2))
-    elif '-' in polecenie:
-        tablica1 = zmienNaTablice(polecenie.split('-')[0])
-        tablica2 = zmienNaTablice(polecenie.split('-')[1])
-        print(roznica(tablica1, tablica2))
+def collection_operations(math_operation):
+    if '*' in math_operation:
+        list1 = convertToList(math_operation.split('*')[0])
+        list2 = convertToList(math_operation.split('*')[1])
+        print(commonpart(list1, list2))
+    elif '+' in math_operation:
+        list1 = convertToList(math_operation.split('+')[0])
+        list2 = convertToList(math_operation.split('+')[1])
+        print(both(list1, list2))
+    elif '-' in math_operation:
+        list1 = convertToList(math_operation.split('-')[0])
+        list2 = convertToList(math_operation.split('-')[1])
+        print(difference(list1, list2))
 
 if __name__ == '__main__':
-    operacjeNaZbiorach('[5,3,8]*[1,0,3]')
-    operacjeNaZbiorach('[5,3,8]+[1,0,3]')
-    operacjeNaZbiorach('[5,3,8]-[1,0,3]')
+    def check_coll(coll):
+        if coll[0] == "[" and coll[-1] == "]":
+            numbers = convertToList(coll.strip())
+            for number in numbers:
+                if not number.isdigit():
+                    return False
+            return True
+        else:
+            return False
+    operation = input("Podaj operacje: ")
+    if operation[0] == '[' and operation[-1] == ']':
+        if '*' in operation:
+            if '+' not in operation and '-' not in operation:
+                if check_coll(operation.split('*')[0]) and check_coll(operation.split('*')[1]):
+                    collection_operations(operation)
+                else:
+                    print('Podano nieprawidlowe dane')
+            else:
+                print('Podano nieprawidlowe dane')
+        elif '+' in operation:
+            if '*' not in operation and '-' not in operation:
+                if check_coll(operation.split('+')[0]) and check_coll(operation.split('+')[1]):
+                    collection_operations(operation)
+                else:
+                    print('Podano nieprawidlowe dane')
+            else:
+                print('Podano nieprawidlowe dane')
+        elif '-' in operation:
+            if '+' not in operation and '*' not in operation:
+                if check_coll(operation.split('-')[0]) and check_coll(operation.split('-')[1]):
+                    collection_operations(operation)
+                else:
+                    print('Podano nieprawidlowe dane')
+            else:
+                print('Podano nieprawidlowe dane')
+        else:
+            print('Podano nieprawidlowe dane')
+    else:
+        print('Podano nieprawidlowe dane')
+    collection_operations('[2,3,4]*[2,3]')
+    collection_operations('[2,3,4]+[2,3]')
+    collection_operations('[2,3,4]-[2,3]')
