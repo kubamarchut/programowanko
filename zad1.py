6e39fad588f66f2280e22a47b11b1360d3ac5e64
kubamarchut <43681491+kubamarchut@users.noreply.github.com>
2021-12-02 09:09:51 +0100

    Add files via upload

--- zad1.py
+++ zad1.py
@@ -1,42 +1,35 @@
-def isbn_10(isbn):
-    ctrlsum = 0
-    for index, num in enumerate(isbn[:-1]):
-        ctrlsum += (index + 1) * int(num)
+class dlugosc:
+    def __init__(self, dlugosc, jednostka = "m"):
+        self.dlugosc = dlugosc
+        self.jednostka = jednostka
 
-    return ctrlsum % 11 == int(isbn[-1])
+    def __str__(self):
+        return str(self.dlugosc) + " " + self.jednostka
 
-def isbn_13(isbn):
-    ctrlsum = 0
-    for index, num in enumerate(isbn[:-1]):
-        waga = 1 if index % 2 == 0 else 3
-        ctrlsum += waga * int(num)
+    def convert_to_m(self):
+        if self.jednostka == "mm":
+            return self.dlugosc / 1000
+        elif self.jednostka == "cm":
+            return self.dlugosc / 100
+        elif self.jednostka == "m":
+            return self.dlugosc
+        elif self.jednostka == "km":
+            return self.dlugosc * 1000
 
-    ctrlsum = 10 - ctrlsum % 10
-    return ctrlsum % 10 == int(isbn[-1])
+    def __add__(self, other):
+        return dlugosc(self.convert_to_m() + other.convert_to_m())
 
-
-def sprawdzanie_isbn(isbn):
-    isbn = isbn.replace("-", "")
-    isbn_length = len(isbn)
-    if isbn_length == 10 or isbn_length == 13:
-        if isbn_length == 10:
-            if isbn_10(isbn):
-               print('Ten ISBN-10 jest prawidłowy')
-            else:
-               print('Ten ISBN-10 nie jest prawidłowy')
-        elif isbn_length == 13:
-            if isbn_13(isbn):
-                print('Ten ISBN-13 jest prawidłowy')
-            else:
-                print('Ten ISBN-13 nie jest prawidłowy')
-
-    else:
-        print("Prawidłowy kod ISBN musi mieć 10 lub 13 cyfr")
-
+    def __sub__(self, other):
+        return dlugosc(self.convert_to_m() - other.convert_to_m())
 
 if __name__ == '__main__':
-    sprawdzanie_isbn(input("Podaj numer ISBN: "))
-    sprawdzanie_isbn("0-306-40615-2")
-    sprawdzanie_isbn("978-3-16-148410-0")
-    sprawdzanie_isbn("978-3-16-1480-0")
-    sprawdzanie_isbn("938-3-16-142341-0")
+    x = dlugosc(2)
+    y = dlugosc(4000, "mm")
+    z = dlugosc(100, "cm")
+
+    print("x = ", str(x))
+    print("y = ", str(y))
+    print("z = ", str(z))
+
+    print("suma x + y + z =", str(x + y + z))
+    print("suma x + y - z =", str(x + y - z))
